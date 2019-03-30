f8ef55f4b57a45fe1eb017ca148bda1624315da1
Lovely Fellow <44167585+Lovely-Fellow@users.noreply.github.com>
2019-03-30 19:41:47 +0100

    DrawOnScroll

---
+++ DrawOnScroll/code.js
@@ -0,0 +1,11 @@
+$( document ).ready(function() {
+var $doc = $(document),
+    $win = $(window),
+    $svg = $('svg').drawsvg(),
+    max = $doc.height() - $win.height();
+
+$win.on('scroll', function() {
+  var p = $win.scrollTop() / max;
+  $svg.drawsvg('progress', p);
+});
+});
