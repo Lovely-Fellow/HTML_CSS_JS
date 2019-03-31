a23e1596a03bf55c049cf8abeab0888f6a88b35d
Lovely Fellow <44167585+Lovely-Fellow@users.noreply.github.com>
2019-03-31 22:03:39 +0200

    GetInstagramProfile

---
+++ Social/GetInstagramProfilePhotoUrl/code.js
@@ -0,0 +1,19 @@
+
+	function get_instagram_profile_url(name)
+{
+
+	$.ajax({
+	type: 'GET',
+	 url: 'https://www.instagram.com/' + name,
+	 success: function (data) {
+	  data = JSON.parse(data.split("window._sharedData = ")[1].split(';')[0]).entry_data.ProfilePage[0];
+	  usr_id = data.graphql.user["id"];	
+	  $.getJSON("https://i.instagram.com/api/v1/users/"+usr_id+"/info/", function(info) {
+		path = info.user.hd_profile_pic_url_info["url"];
+		console.log(path);
+		$("#profile").attr("src", path);
+		return path;
+	  });
+	 }
+	});
+	}
