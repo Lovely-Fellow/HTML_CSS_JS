a5082f95045f0ff83c2530002945d749881f0ecb
Lovely Fellow <44167585+Lovely-Fellow@users.noreply.github.com>
2019-04-30 08:00:17 +0200

    setting the cursor

---
+++ Setting the cursor position of Text with JavaScript/code.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+	// Credits: http://blog.vishalon.net/index.php/javascript-getting-and-setting-caret-position-in-textarea/
+function setCaretPosition(ctrl, pos) {
+  // Modern browsers
+  if (ctrl.setSelectionRange) {
+    ctrl.focus();
+    ctrl.setSelectionRange(pos, pos);
+  
+  // IE8 and below
+  } else if (ctrl.createTextRange) {
+    var range = ctrl.createTextRange();
+    range.collapse(true);
+    range.moveEnd('character', pos);
+    range.moveStart('character', pos);
+    range.select();
+  }
+}
+
+// Set the cursor position of the "#test-input" element to the end when the page loads
+var input = document.getElementById('test-input');
+setCaretPosition(input, 3);
+});
